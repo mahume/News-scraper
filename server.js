8d74ecc66fa81d313cd3dcd7dfefcc4dd72f052a
mahume <michael.a.hume@gmail.com>
2019-04-23 15:56:04 -0600

    adding directories

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const db = require('./models');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+app.use(express.static('public'));
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
