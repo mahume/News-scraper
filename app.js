5f768aa80679e61e8224ddba6779f645605e7bee
mahume <michael.a.hume@gmail.com>
2019-04-26 20:59:24 -0600

    Adding Handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 const routes = require('./routes/routes');
 
 const app = express();
@@ -9,6 +10,9 @@ mongoose.connect('mongodb://localhost/newsdb', { useNewUrlParser: true });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 routes(app);
 
